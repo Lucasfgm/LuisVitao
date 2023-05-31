f57095830b3765558313953d5a13b065726a4d0f
Lucas Flávio <lucasfgm00@gmail.com>
2023-05-30 22:20:43 -0300

    DAOs

---
+++ src/main/java/ifnmg/edu/com/br/luisvitao/ProdutoDAO.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package ifnmg.edu.com.br.luisvitao;
+
+/**
+ *
+ * @author Lucas Flavio<lucasfgm at ifnmg.edu.br>
+ */
+public interface ProdutoDAO {
+    
+    public void salvar (Produto produto);
+    
+}
